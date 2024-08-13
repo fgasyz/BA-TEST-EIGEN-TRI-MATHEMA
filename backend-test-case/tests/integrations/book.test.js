ebaac71df483a67f914544c0bfa4553f2301bec3
fgasyz <ismayozaf@gmail.com>
2024-08-14 01:09:49 +0700

    finish

--- backend-test-case/tests/integrations/book.test.js
+++ backend-test-case/tests/integrations/book.test.js
@@ -1,6 +1,1 @@
-describe('books', () => {
-  describe('Check the book', () => {
-    it('Shows all existing books and quantities', () => {});
-    it('Books that are being borrowed are not counted', () => {});
-  });
-});
+describe('books', () => {});
